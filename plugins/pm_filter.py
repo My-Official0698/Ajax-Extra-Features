e96f9c35a6050f13fb2303d21da16f1c5b7d7d65
DesiBots <103522724+DesiBots@users.noreply.github.com>
2022-05-30 10:44:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -151,7 +151,7 @@ async def give_filter(client,message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("𝗛𝗲𝘆 𝗙𝗿𝗶𝗲𝗻𝗱 , 𝗗𝗼𝗻'𝘁 𝗧𝗮𝗸𝗲 𝗼𝘁𝗵𝗲𝗿𝘀 𝗣𝗿𝗼𝗽𝗲𝗿𝘁𝘆...😁 𝗦𝗲𝗮𝗿𝗰𝗵 𝘆𝗼𝘂𝗿 𝗢𝘄𝗻....😍", show_alert=True)
+        return await query.answer("𝗛𝗲𝘆 𝗙𝗿𝗶𝗲𝗻𝗱 , 𝗗𝗼𝗻'𝘁 𝗧𝗮𝗸𝗲 𝗼𝘁𝗵𝗲𝗿𝘀 𝗣𝗿𝗼𝗽𝗲𝗿𝘁𝘆...😁 𝗦𝗲𝗮𝗿𝗰𝗵 𝘆𝗼𝘂𝗿 𝗢𝘄𝗻....", show_alert=True)
     try:
         offset = int(offset)
     except:
